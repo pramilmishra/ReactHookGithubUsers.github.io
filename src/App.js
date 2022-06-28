6881cd94d2b8cef2c85f48e2e7b5ba70c20cddd6
Pramil Mishra <pramil.345@gmail.com>
2022-06-28 18:48:58 +0530

    Github Users Project Using React Hooks

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import UseEffectApi from './UseEffectApi';
+
+
+const App = () => {
+  return (
+    <div>
+      <UseEffectApi/>
+    </div>
+  )
+}
+
+export default App
